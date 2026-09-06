92cf35df1b8b7298bd2efabcf4ed9e9586175bbd
Tsamh <tsambahama@gmail.com>
2026-09-06 16:47:40 +0000

    style: retrait du BOM et ajout du saut de ligne final dans CounterScreen

--- src/CounterScreen.tsx
+++ src/CounterScreen.tsx
@@ -1,4 +1,4 @@
-﻿import { useState } from 'react';
+import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { decrement, increment, reset } from './counter';
